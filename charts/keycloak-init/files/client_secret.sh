455b15eb39a8be716b3db0a0b4bfa495e67e1adc
Matteo Palladino <pastnullinfinity@gmail.com>
2020-04-27 10:48:58 +0200

    [keycloak-init] fix client_secret bug with kcadm

--- charts/keycloak-init/files/client_secret.sh
+++ charts/keycloak-init/files/client_secret.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+set -e
 
-if [[ -z $NAMESPACES ]]; then
-    echo "ERROR: No namespace defined, aborting..."
-    exit 1
-fi
+# if [[ -z $NAMESPACES ]]; then
+#     echo "ERROR: No namespace defined, aborting..."
+#     exit 1
+# fi
 
-password=$(echo $(</secrets/password))
+password=$KEYCLOAK_PASSWORD
 
-./kcadm.sh config credentials --server $KEYCLOAK_HOST --realm master --user $KEYCLOAK_USER --password $password
+/opt/jboss/keycloak/bin/kcadm.sh config credentials --server "$KEYCLOAK_HOST" --realm master --user $KEYCLOAK_USER --password $password
+echo "Logged in."
 
-clientId=$(./kcadm.sh get clients --server $KEYCLOAK_HOST --realm master | grep security-admin-console -C 2 | grep id | cut -d '"' -f 4 | tr -d '\n')
-clientSecret=$(./kcadm.sh get clients/$clientId/client-secret --server $KEYCLOAK_HOST --realm master | grep value | cut -d '"' -f 4 | tr -d '\n')
+clientId=$(/opt/jboss/keycloak/bin/kcadm.sh get clients --server $KEYCLOAK_HOST --realm master | grep security-admin-console -C 2 | grep id | cut -d '"' -f 4 | tr -d '\n')
+clientSecret=$(/opt/jboss/keycloak/bin/kcadm.sh get clients/$clientId/client-secret --server $KEYCLOAK_HOST --realm master | grep value | cut -d '"' -f 4 | tr -d '\n')
 
 delimiter=","
 declare -a namespaces=($(echo $NAMESPACES | tr "$delimiter" " "))
 
 for namespace in  ${namespaces[@]}
-do
+do  
+    echo "Setting secret for $namespace"
     kubectl create secret -n "$namespace" generic oidc-client-secret --from-literal=CLIENT_ID="$clientId" --from-literal=CLIENT_SECRET="$clientSecret"
 done
